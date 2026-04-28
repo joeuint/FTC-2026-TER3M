5bd7f7ca0b168e65cf247b922ef4f6b7a404ae91
Anastasiia <anastasiia.koval.822@gmail.com>
2026-04-28 11:23:13 -0400

    Robot oriented drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinOpMode.java
@@ -1,4 +1,72 @@
-package org.firstinspires.ftc.teamcode;
-
-public class SpinOpMode {
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//
+//import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.IMU;
+//
+//@Autonomous
+//public class SpinOpMode extends LinearOpMode {
+//    // var init
+//    DcMotor FrontLeftDrive;
+//    DcMotor FrontRightDrive;
+//    DcMotor BackLeftDrive;
+//    DcMotor BackRightDrive;
+//    @Override
+//        public void runOpMode(){
+//            // initialization
+//            FrontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+//            FrontRightDrive = hardwareMap.get(DcMotor.class, "frontRight");
+//            BackLeftDrive = hardwareMap.get(DcMotor.class, "backLeft");
+//            BackRightDrive = hardwareMap.get(DcMotor.class, "backRight");
+//
+//            FrontRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+//            BackRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+////
+////            IMU imu = hardwareMap.get(IMU.class, "imu");
+////            IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+////                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+////                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+////            imu.initialize(parameters);
+//
+//
+//        // stops until it starts
+//            waitForStart();
+//
+//            while (opModeIsActive()) {
+//                // loop after initialization
+//                if (gamepad1.){
+//                    FrontLeftDrive.setPower(1);
+//                    FrontRightDrive.setPower(1);
+//                    BackLeftDrive.setPower(1);
+//                    BackRightDrive.setPower(1);
+//                    telemetry.addLine("A button is pressed: moving forward");
+//                }
+//                else if (gamepad1.) {
+//                    FrontLeftDrive.setPower(0);
+//                    FrontRightDrive.setPower(1);
+//                    BackLeftDrive.setPower(0);
+//                    BackRightDrive.setPower(1);
+//                    telemetry.addLine("*** button is pressed: moving left");
+//                }
+//                else if (gamepad1.) {
+//                    FrontLeftDrive.setPower(1);
+//                    FrontRightDrive.setPower(0);
+//                    BackLeftDrive.setPower(1);
+//                    BackRightDrive.setPower(0);
+//                    telemetry.addLine("*** button is pressed: moving right");
+//                }
+//                else{
+//                    FrontLeftDrive.setPower(0);
+//                    FrontRightDrive.setPower(0);
+//                    BackLeftDrive.setPower(0);
+//                    BackRightDrive.setPower(0);
+//                    telemetry.addLine("No movement");
+//                }
+//            }
+//    }
+//}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Teleop extends LinearOpMode {
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+
+    boolean isFieldOriented = false;
+
+@Override
+    public void runOpMode() {
+
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeft");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRight");
+
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        while(opModeIsActive()) {
+            if (gamepad1.aWasPressed()){
+                isFieldOriented = true;
+            }
+            if (isFieldOriented) {
+                driveFieldOriented(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            }
+
+            if (gamepad1.bWasPressed()){
+                isFieldOriented = false;
+            }
+            if(!isFieldOriented){
+                drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            }
+
+        }
+    }
+    public void drive(double forward, double right, double rotate) {
+        double frontLeftPower = forward + right + rotate;
+        double frontRightPower = forward - right - rotate;
+        double backRightPower = forward + right - rotate;
+        double backLeftPower = forward - right + rotate;
+
+        frontLeftDrive.setPower(frontLeftPower);
+        frontRightDrive.setPower(frontRightPower);
+        backLeftDrive.setPower(backLeftPower);
+        backRightDrive.setPower(backRightPower);
+    }
+
+    public void driveFieldOriented(double forward, double right, double rotate){
+
+    }
+
+}
