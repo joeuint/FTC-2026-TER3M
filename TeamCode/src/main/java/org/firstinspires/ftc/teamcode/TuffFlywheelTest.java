29ce49b6afb3bcbd7474eaf5fb9dadd1e1078e4c
Joseph Umana <joseph@josephumana.com>
2026-06-04 14:25:51 -0400

    Update FlyWheel Test OpMode
    Signed-off-by: Joseph Umana <joseph@josephumana.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuffFlywheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuffFlywheelTest.java
@@ -2,33 +2,31 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous
+@TeleOp
 public class TuffFlywheelTest extends LinearOpMode {
     DcMotor shooter;
     ElapsedTime dt = new ElapsedTime();
-    double lastTicks = 0;
 
     @Override
     public void runOpMode() {
         shooter = hardwareMap.get(DcMotor.class, "shooter");
-
-        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         shooter.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
+
+        shooter.setPower(0.5);
         dt.reset();
         while (opModeIsActive()) {
-            shooter.setPower(1);
-            double rotations = Math.abs(shooter.getCurrentPosition() / 280.0);
-            telemetry.addData("EncoderTicks", rotations - lastTicks/ dt.seconds());
-
+            if (gamepad1.x) {
+                shooter.setPower(Math.abs(gamepad1.left_stick_x));
+            }
+            telemetry.addData("Shoot Power", shooter.getPower());
             telemetry.update();
-            lastTicks = rotations;
-            dt.reset();
         }
     }
 }
